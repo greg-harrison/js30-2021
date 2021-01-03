27258064e8a5c426cb5700577ce8755c2f51dcd4
Greg <harrison.greg.c@gmail.com>
2021-01-03 18:40:36 -0500

    Day One - BG image added

--- gh-pages-js302021/src/pages/dayOne.js
+++ gh-pages-js302021/src/pages/dayOne.js
@@ -1,16 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DayOneCrowd from "../images/d1-background.jpg"
 
-const SecondPage = () => (
+const DayOne = () => {
+return (
   <Layout>
     <SEO title="day one" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div style={{ 
+        background: `url(${DayOneCrowd})`,
+        height: '100vh'
+      }}>
+        
+    </div>
   </Layout>
 )
+}
 
-export default SecondPage
+export default DayOne
