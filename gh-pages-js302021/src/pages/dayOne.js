88b25b1052dd6386749be3334f6ae6def94cb9ce
Greg <harrison.greg.c@gmail.com>
2021-01-03 19:05:24 -0500

    Working on getting audio tag to play audio src

--- gh-pages-js302021/src/pages/dayOne.js
+++ gh-pages-js302021/src/pages/dayOne.js
@@ -1,20 +1,43 @@
-import React from "react"
+import React, { cloneElement } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DayOneCrowd from "../images/d1-background.jpg"
+import boom from "../assets/dayone/sounds/boom.wav"
+import styled from "styled-components"
+
+const Container = styled.div({
+  background: `url(${DayOneCrowd})`,
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+});
+
+const EmphasizedText = styled.span({
+  color: "#e4ff00"
+})
+
+const onKeyPressed = (e) => {
+  console.log(e)
+  const myRef = React.createRef();
+  const audio = new Audio(boom);
+
+  audio.play();
+  return (
+    <audio ref={myRef} src={boom} autoPlay/>
+  )
+}
 
 const DayOne = () => {
-return (
-  <Layout>
-    <SEO title="day one" />
-    <div style={{ 
-        background: `url(${DayOneCrowd})`,
-        height: '100vh'
-      }}>
-        
-    </div>
-  </Layout>
-)
+  return (
+    <Layout>
+      <SEO title="day one" />
+      <Container>
+        Test
+        <button onKeyDown={onKeyPressed}> HELLO </button>
+      </Container>
+    </Layout>
+  )
 }
 
 export default DayOne
